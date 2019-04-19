528fcfcadd712463b6fdcc2b15b7217cac3e3afa
Jirawat Boonsumret <jirawat859@hotmail.com>
2019-04-19 20:42:12 +0700

    [add] overall routes

---
+++ navigation/Routes.js
@@ -0,0 +1,39 @@
+//import liraries
+import React, { Component } from "react";
+import { Router, Scene } from "react-native-router-flux";
+import LoginPage from "../pages/Login";
+import RegisterPage from "../pages/Restaurant";
+import EditUserPage from "../pages/EditUser";
+import HomePage from "../pages/Home";
+import RestaurantPage from "../pages/Restaurant";
+import RestaurantDetailPage from "../pages/RestaurantDetail";
+import AddRestaurantPage from "../pages/AddRestaurant";
+import ReviewDetailPage from "../pages/ReviewDetail";
+import SearchPage from "../pages/Search";
+import UserProfilePage from "../pages/UserProfile";
+
+// create a component
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="Login" component={LoginPage} initial />
+          <Scene key="Register" component={RegisterPage} />
+          <Scene key="EditUser" component={EditUserPage} />
+          <Scene key="Home" component={HomePage} />
+          <Scene key="Restaurant" component={RestaurantPage} />
+          <Scene key="RestaurantDetail" component={RestaurantDetailPage} />
+          <Scene key="AddRestaurant" component={AddRestaurantPage} />
+          <Scene key="ReviewDetail" component={ReviewDetailPage} />
+          <Scene key="AddReview" component={AddReviewPage} />
+          <Scene key="Search" component={SearchPage} />
+          <Scene key="UserProfile" component={UserProfilePage} />
+        </Scene>
+      </Router>
+    );
+  }
+}
+
+//make this component available to the app
+export default Routes;
